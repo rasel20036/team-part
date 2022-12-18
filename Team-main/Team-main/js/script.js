99371f452f68f447eb908df9e5307026dc1df6f3
rasel20036 <120622520+rasel20036@users.noreply.github.com>
2022-12-18 11:59:18 +0600

    Add files via upload

---
+++ Team-main/Team-main/js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.item_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows:false,
+    })    
+})
